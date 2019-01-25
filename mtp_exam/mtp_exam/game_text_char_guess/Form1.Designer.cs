0b1252a5504529aaf10d8790f5f04a2b3292b865
adrianB3 <balanescuadrian71@gmail.com>
2019-01-25 23:03:47 +0200

    added prob 1

---
+++ mtp_exam/mtp_exam/game_text_char_guess/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace game_text_char_guess
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.scorLabel = new System.Windows.Forms.Label();
+            this.inputTb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.validationLabel = new System.Windows.Forms.Label();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(177, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(310, 365);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Scor";
+            // 
+            // scorLabel
+            // 
+            this.scorLabel.AutoSize = true;
+            this.scorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorLabel.ForeColor = System.Drawing.Color.LightSalmon;
+            this.scorLabel.Location = new System.Drawing.Point(74, 22);
+            this.scorLabel.Name = "scorLabel";
+            this.scorLabel.Size = new System.Drawing.Size(43, 25);
+            this.scorLabel.TabIndex = 2;
+            this.scorLabel.Text = "0%";
+            // 
+            // inputTb
+            // 
+            this.inputTb.Location = new System.Drawing.Point(29, 82);
+            this.inputTb.Name = "inputTb";
+            this.inputTb.Size = new System.Drawing.Size(100, 20);
+            this.inputTb.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.GhostWhite;
+            this.button1.Location = new System.Drawing.Point(12, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 69);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // validationLabel
+            // 
+            this.validationLabel.AutoSize = true;
+            this.validationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.validationLabel.Location = new System.Drawing.Point(32, 210);
+            this.validationLabel.Name = "validationLabel";
+            this.validationLabel.Size = new System.Drawing.Size(0, 29);
+            this.validationLabel.TabIndex = 5;
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Location = new System.Drawing.Point(14, 59);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(0, 13);
+            this.inputLabel.TabIndex = 6;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 387);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.validationLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inputTb);
+            this.Controls.Add(this.scorLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Main";
+            this.Text = "Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scorLabel;
+        private System.Windows.Forms.TextBox inputTb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label validationLabel;
+        private System.Windows.Forms.Label inputLabel;
+    }
+}
+
---
+++ mtp_exam/mtp_exam/game_text_char_guess/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace game_text_char_guess
+{
+    public partial class Main : Form
+    {
+        private string text;
+        private int count = 0;
+        private float score = 0;
+        public Main()
+        {
+            InitializeComponent();
+            inputLabel.Text = "Input new character (just one!)";
+            var fs = new StreamReader(new FileStream("rand_text.txt", FileMode.Open));
+            text = fs.ReadToEnd();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(count < text.Length)
+            {
+                if (inputTb.Text.ToString().ToLower().Equals(text.ElementAt(count).ToString().ToLower()))
+                {
+                    validationLabel.Text = "Yep";
+                    textBox1.AppendText(text[count].ToString());
+                    score++;
+                    float scorePrc = (100 * score) / text.Length;
+                    scorLabel.Text = scorePrc.ToString() + " %";
+                }
+                else
+                {
+                    validationLabel.Text = "Nope";
+                    textBox1.AppendText(text[count].ToString());
+                }
+                count++;
+                inputTb.Clear();
+                inputTb.Focus();
+            }
+            if(count == text.Length)
+            {
+                button1.Enabled = false;
+                validationLabel.Text = "End of text";
+            }
+        }
+    }
+}
